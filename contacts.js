f3d1066062f639700f231df112481f8047ddad65
andreyz11288 <andreyz11288@gmail.com>
2021-04-06 16:39:11 +0300

    refactor

--- contacts.js
+++ contacts.js
@@ -3,36 +3,29 @@ const path = require('path')
 
 const contactsPath = path.resolve('./db/contacts.json')
 
-// TODO: задокументировать каждую функцию
-function listContacts () {
-  fs.readFile(`${contactsPath}`, 'utf8', (error, data) => {
-    if (error) console.log(error) // если возникла ошибка
-    const obj = JSON.parse(data)
+const contents = fs.readFileSync(`${contactsPath}`, 'utf8')
 
-    console.table(obj) // выводим считанные данные
-  })
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  const contact = JSON.parse(contents)
+  console.table(contact) // выводим считанные данные
 }
 
 function getContactById (contactId) {
-  // ...твой код
-  fs.readFile(`${contactsPath}`, 'utf8', (error, data) => {
-    if (error) console.log(error) // если возникла ошибка
-    const obj = JSON.parse(data)
-    // eslint-disable-next-line array-callback-return
-    obj.map((data) => {
-      if (`${data.id}` === contactId) {
-        console.log(data)
-      }
-    })
+  // ...твой код  
+  const contact = JSON.parse(contents)
+  contact.map((data) => {
+    if (`${data.id}` === contactId) {
+      console.log(data)
+    }
   })
+  
 }
 
 function removeContact (contactId) {
   // ...твой код
-  fs.readFile(`${contactsPath}`, 'utf8', (error, data) => {
-    if (error) console.log(error) // если возникла ошибка
-    const obj = JSON.parse(data)
-    const arr = obj.filter((data) => `${data.id}` !== contactId)
+    const contact = JSON.parse(contents)
+    const arr = contact.filter((data) => `${data.id}` !== contactId)
     fs.writeFile(
       `${contactsPath}`,
       `${JSON.stringify(arr, null, 4)}`,
@@ -45,34 +38,30 @@ function removeContact (contactId) {
         })
       }
     )
-  })
+  
 }
 
 const addContact = (name, email, phone) => {
   // ...твой код
 
-  fs.readFile(`${contactsPath}`, 'utf8', function (error, data) {
-    if (error) console.log(error) // если возникла ошибка
-    const obj = JSON.parse(data)
-    // console.log('obj.length', obj.length)
-    const contactId = obj.length + 1
+  const contact = JSON.parse(contents)
+    const contactId = contact.length + 1
     const allJson = [
-      ...obj,
+      ...contact,
       { id: contactId, name: `${name}`, email: `${email}`, phone: `${phone}` },
     ]
-    fs.writeFile(
-      `${contactsPath}`,
-      `${JSON.stringify(allJson, null, 4)}`,
-      function (error) {
+  fs.writeFile(
+    `${contactsPath}`,
+    `${JSON.stringify(allJson, null, 4)}`,
+    function (error) {
+      if (error) console.log(error) // если возникла ошибка
+      fs.readFile(`${contactsPath}`, 'utf8', (error, data) => {
         if (error) console.log(error) // если возникла ошибка
-        fs.readFile(`${contactsPath}`, 'utf8', (error, data) => {
-          if (error) console.log(error) // если возникла ошибка
-          console.table(JSON.parse(data))
-        })
-        // выводим считанные данные
-      }
-    )
-  })
+        console.table(JSON.parse(data))
+      })
+    }
+  )
+  
 }
 module.exports = {
   listContacts,
